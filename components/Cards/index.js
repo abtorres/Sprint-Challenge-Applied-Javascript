4d4a9e3781cd765eda9539ef10b2d7a1a7dae0e4
abtorres <abtorres079@gmail.com>
2019-07-20 12:08:57 -0600

    add article card/ mvp achieved

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,54 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(function (response) {
+    // handle success
+    const articles = response.data;
+    // console.log(articles)
+    article(articles);    
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+
+function article(articles) {
+    const a = articles.articles;
+    // console.log(a);
+    for (var key in a) {
+        // console.log(key, a[key]);
+        a[key].forEach(element => {
+            // console.log(element);
+            const card = document.createElement('div');
+            card.classList.add('card');
+
+            const headline = document.createElement('div');
+            headline.classList.add('headline');
+            headline.textContent = element.headline;
+            card.appendChild(headline);
+
+            const author = document.createElement('div');
+            author.classList.add('author');
+            author.textContent = element.authorName;
+            card.appendChild(author);
+
+            const imageContainer = document.createElement('div');
+            imageContainer.classList.add('img-container');
+            author.appendChild(imageContainer);
+
+            const authorImage = document.createElement('img');
+            authorImage.src = element.authorPhoto;
+            imageContainer.appendChild(authorImage);
+
+            const authorName = document.createElement('span');
+            authorName.textContent = element.authorName;
+            author.appendChild(authorName);
+
+            // console.log(card)
+            const mountPoint = document.querySelector('.cards-container');
+            mountPoint.appendChild(card);
+        });
+      }
+}
