7e7f2c796efea58b6723dabde85bd090ad427b74
abtorres <abtorres079@gmail.com>
2019-07-20 10:21:00 -0600

    Done with header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,38 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+    const header = document.createElement('div');
+    header.classList.add('header');
+
+    const date = document.createElement('span');
+    date.classList.add('date');
+    date.textContent = dateFormat();
+    header.appendChild(date);
+
+    const title = document.createElement('h1');
+    title.textContent = 'Lambda Times';
+    header.appendChild(title);
+
+    const temp = document.createElement('span');
+    temp.classList.add('temp');
+    temp.textContent = '98°';
+    header.appendChild(temp);
+
+    return header;
+}
+
+//format date to show m/d/y
+function dateFormat() {
+    const currDate = new Date();
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const month = months[currDate.getMonth()];
+    const day = currDate.getDate();
+    const year = currDate.getFullYear();
+    const d = month + ' ' + day + ", " + year;
+
+    return d;
+}
+
+const headerContainer = document.querySelector('.header-container');
+headerContainer.appendChild(Header());
